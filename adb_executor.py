1c0f1c2a5d697c0919b339dad7a9c3be8a946fec
Daniel Morawski <gvrmvnn@gmail.com>
2024-03-20 19:43:44 +0100

    Add pulling by specified media

--- adb_executor.py
+++ adb_executor.py
@@ -1,3 +1,4 @@
+import json
 import subprocess
 import os
 from logger import *
@@ -5,23 +6,37 @@ from logger import *
 logger = logging.getLogger(__name__)
 
 
-class ADB:
+class ExecutorADB:
+    def __init__(self):
+        with open('configs/adbexecutor.json', 'r') as file:
+            config = json.load(file)
+        self.medias = config['medias']
+        self.execute = config['execute']
 
     def pull(self, path):
         # TODO: Pulling by loop using json configure
         # Make path as absolute
         destination_path = os.path.abspath(path)
 
-        # Create directory if not existance
+        # Create directory if not exists
         os.makedirs(destination_path) if not os.path.exists(destination_path) else None
 
-        command = f"adb pull /sdcard/DCIM/Camera/IMG_20231026_204815.jpg {destination_path}"
-        result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='utf-8')
+        results = []
+        for media, source in self.medias.items():
+            if self.execute[media]:
+                command = f"adb pull {source} {destination_path}"
+                result = subprocess.run(command,
+                                        shell=True,
+                                        stdout=subprocess.PIPE,
+                                        stderr=subprocess.PIPE,
+                                        encoding='utf-8')
 
-        if result.returncode == 0:
-            return success("Process result: " + result.stdout)
-        else:
-            return failure(f"Error occur while pulling media: {result.stderr}")
+                if result.returncode == 0:
+                    results.append(success(f"Pulling from {media}: " + result.stdout))
+                else:
+                    results.append(failure(f"Error occur while pulling {media}: {result.stdout}"))
+        return success("Pulling OK") if all(results) else failure("Pulling NOK")
+
     def _check(self):
         pass
 
@@ -31,5 +46,5 @@ class ADB:
 
 
 if __name__ == "__main__":
-    adb = ADB()
+    adb = ExecutorADB()
     adb.pull("C:/Users/Daniel/Desktop/test")
